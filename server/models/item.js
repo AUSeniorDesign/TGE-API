9eb5bd1c6cc41e3ccb471a2f5aece3d998a6f558
Haven Barnes <haven38@gmail.com>
2018-01-24 13:26:07 -0600

    added inventory field to Item model

--- server/models/item.js
+++ server/models/item.js
@@ -2,6 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
     var Item = sequelize.define('Item', {
       name: DataTypes.STRING,
+      // Choosing a floating point for inventory field to be safe on the OFF chance something is sold in a weight unit
+      inventory: DataTypes.DECIMAL(10, 2),
       price: DataTypes.DECIMAL(10, 2),
       shippingCost: DataTypes.DECIMAL(10, 2),
       // TODO: Make condition an ENUM type once we get a defined list
