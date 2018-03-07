81f4a6b93a1aeb16b053a595a6816afd3cdc5927
Haven Barnes <haven38@gmail.com>
2018-03-07 08:24:09 -0600

    New Arrival CRUD services implemented

---
+++ server/routes/feed.js
@@ -0,0 +1,80 @@
+/**
+ * @author Haven Barnes <hab0020@auburn.edu>
+ */
+
+const express = require("express");
+const NewArrivalPost = require("../models").NewArrivalPost;
+
+// New Arrival Post Endpoints
+module.exports = function(app, passport) {
+  var router = express.Router();
+
+  // Get all new arrival posts, sorted by date
+  router.get("/", passport.isEmployee, function(req, res, next) {
+    NewArrivalPost.findAll({ order: ["id", "DESC"] })
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  // Create New Arrival Post
+  /**
+   * Employees Only.
+   * 
+   *  Body MUST be in the following format:
+   *  req.body = {
+   *      post: {
+   *          description: "Here's a rare collectible that came in today",
+   *          images: "http://image.com/collectible_1", "http://image.com/collectible_2"
+   *          store: "Charlotte Avenue Superstore"
+   *      }
+   *  }
+   *
+   */
+  router.post("/", passport.isEmployee, function(req, res, next) {
+    NewArrivalPost.create(req.body)
+      .then(posts => {
+        res.status(200).json(post);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  // Update New Arrival Post
+  // Admins / Employees Only
+  router.put("/:id", passport.isEmployee, function(req, res, next) {
+    NewArrivalPost.update(req.body.post, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(updatedRecords => {
+        res.status(200).json(updatedRecords);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  // Delete New Arrival Post
+  // Admins / Employees Only
+  router.delete("/:id", passport.isEmployee, function(req, res, next) {
+    NewArrivalPost.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(deletedRecords => {
+        res.status(200).json(deletedRecords);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  app.use("/items", router);
+};
