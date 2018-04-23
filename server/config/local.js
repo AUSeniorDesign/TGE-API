cff2a02dcf5296f916b619adb505a95933677990
Haven Barnes <haven38@gmail.com>
2018-04-23 10:10:17 -0500

    fixed local.js which was causing build issue

--- server/config/local.js
+++ server/config/local.js
@@ -1,9 +1,3 @@
-
-process.env = {
-  NODE_ENV: "development",
-  // PORT : 3000,
-  FB_APP_ID : "325985624578151",
-  FB_APP_SECRET : "6dfa4f79635c4583a8614d4d8ee5514f",
-  USERNAME: "admin",
-  PASSWORD:"admin"
-};
+module.exports = {
+"port": 3000	
+}
